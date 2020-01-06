2e2118d8743ed67a73d802f30107e4b7478fc9f2
Grimiri <47869932+grimiri@users.noreply.github.com>
2020-01-06 03:07:49 +0300

    push

---
+++ src/Algorithmization_2/arrays/Task_1.java
@@ -0,0 +1,26 @@
+package Algorithmization_2.arrays;
+
+import java.util.Scanner;
+/*
+* 1. В массив A [N] занесены натуральные числа. Найти сумму тех элементов, которые кратны данному К.
+* */
+public class Task_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter k..");
+        int k = scanner.nextInt();
+        int sum = 0;
+        int counter = 0;
+        int[] array = new int[10];
+        System.out.print("array: ");
+        for(int i = 0; i < array.length; i++){
+            array[i] = (int)(Math.random() * 10);
+            System.out.print(array[i] + " ");
+            if (array[i] % k == 0 && counter < 3){
+                sum += array[i];
+                counter++;
+            }
+        }
+        System.out.println("\nanswer: sum = " + sum);
+    }
+}
